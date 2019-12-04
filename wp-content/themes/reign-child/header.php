42aa23cf4cd19c6a9651c5e7026470bdf04fc003
Nilam Nilam <nilam@cmsminds.com>
2019-12-04 19:27:55 +0530

    google_analytics

--- wp-content/themes/reign-child/header.php
+++ wp-content/themes/reign-child/header.php
@@ -14,9 +14,20 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-P8KHMDH');</script>
+		<!-- End Google Tag Manager -->	
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8KHMDH"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
 		<?php do_action( 'wbcom_before_page' ) ?>
 		<div id="page" class="site">
 			<?php do_action( 'wbcom_before_masthead' ); ?>
