226d32e591912ab40a988da3fdbbf5b1c1e8415a
Alyson Osborn <aly@handsomemeatball.com>
2020-01-08 17:16:27 +0100

    fix array

--- wp-content/themes/reign-child/dokan/store.php
+++ wp-content/themes/reign-child/dokan/store.php
@@ -104,7 +104,7 @@ get_header( 'shop' );
                     <?php }
 			echo '<div class="subhead-cat">';
             echo $store_info['cuisine_spec'];
-            echo $store_info['categories'];
+            echo implode(",", $store_info['categories']);
 			echo '</div>';
 			echo '<div class="contact-vendor">';
 			echo '<a class="venobox button" data-vbtype="inline" href="#contactvendor">Contact</a>';
