6d61e3eba7bccac67e9c8bdc5d4c4bc9a539b8b3
Alyson Osborn <aly@handsomemeatball.com>
2020-01-08 17:57:32 +0100

    Remove chef categories. Not working right.

--- wp-content/themes/reign-child/dokan/store.php
+++ wp-content/themes/reign-child/dokan/store.php
@@ -104,7 +104,7 @@ get_header( 'shop' );
                     <?php }
 			echo '<div class="subhead-cat">';
             echo $store_info['cuisine_spec'];
-            echo dokan_store_category( $seller_id, $title );
+  
 			echo '</div>';
 			echo '<div class="contact-vendor">';
 			echo '<a class="venobox button" data-vbtype="inline" href="#contactvendor">Contact</a>';
