32e9b592da1e2bf38942c50f23d6f670d5996582
Alyson Osborn <aly@handsomemeatball.com>
2020-01-14 22:24:25 +0100

    change sizes

--- wp-content/themes/reign-child/includes/toggle-jquery.php
+++ wp-content/themes/reign-child/includes/toggle-jquery.php
@@ -8,40 +8,40 @@
 add_action( 'wp_footer', function () { ?>
 	<script>
 jQuery(document).ready(function () {
-if (jQuery(".biowrapper").height() > 150) {
+if (jQuery(".biowrapper").height() > 156) {
 jQuery('.biowrapper').find('a[href="#"]').on('click', function (e) {
     e.preventDefault();
     this.expand = !this.expand;
     jQuery(this).text(this.expand?"Click to collapse":"Click to read more");
     jQuery(this).closest('.biowrapper').find('.biosmall, .biobig').toggleClass('biosmall biobig');
 });
-} else if (jQuery(".biowrapper").height() < 149) {
+} else if (jQuery(".biowrapper").height() <= 156) {
 jQuery( ".biolink" ).hide();
 jQuery(".biosmall").removeClass('biosmall').addClass('biobig');
 }
 });
 jQuery(document).ready(function () {
-if (jQuery(".sdwrapper").height() > 150) {
+if (jQuery(".sdwrapper").height() > 156) {
 jQuery('.sdwrapper').find('a[href="#"]').on('click', function (e) {
     e.preventDefault();
     this.expand = !this.expand;
     jQuery(this).text(this.expand?"Click to collapse":"Click to read more");
     jQuery(this).closest('.sdwrapper').find('.sdsmall, .sdbig').toggleClass('sdsmall sdbig');
 });
-} else if (jQuery(".sdwrapper").height() < 149){
+} else if (jQuery(".sdwrapper").height() <= 156){
 jQuery( ".sdlink" ).hide();
 jQuery(".sdsmall").removeClass('sdsmall').addClass('sdbig');
 }
 });
 jQuery(document).ready(function () {
-if (jQuery(".ldwrapper").height() > 150) {
+if (jQuery(".ldwrapper").height() > 156) {
 jQuery('.ldwrapper').find('a[href="#"]').on('click', function (e) {
     e.preventDefault();
     this.expand = !this.expand;
     jQuery(this).text(this.expand?"Click to collapse":"Click to read more");
     jQuery(this).closest('.ldwrapper').find('.ldsmall, .ldbig').toggleClass('ldsmall ldbig');
 });
-} else if (jQuery(".ldwrapper").height() < 149) {
+} else if (jQuery(".ldwrapper").height() <= 156) {
 jQuery( ".ldlink" ).hide();
 jQuery(".ldsmall").removeClass('ldsmall').addClass('ldbig');
 }
